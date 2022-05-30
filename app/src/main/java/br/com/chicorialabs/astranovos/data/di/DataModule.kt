43b41620dad455cf529e748f335bd189ad1e690d
Gabriel Fernandes Lemos <gabriel_lemos03@hotmail.com>
2022-05-29 22:39:26 -0300

    Added dependencies for viewModel and Koin

---
+++ app/src/main/java/br/com/chicorialabs/astranovos/data/di/DataModule.kt
@@ -0,0 +1,21 @@
+package br.com.chicorialabs.astranovos.data.di
+
+import br.com.chicorialabs.astranovos.data.repository.MockAPIService
+import br.com.chicorialabs.astranovos.data.repository.PostRepository
+import br.com.chicorialabs.astranovos.data.repository.PostRepositoryImpl
+import org.koin.core.context.loadKoinModules
+import org.koin.core.module.Module
+import org.koin.dsl.module
+
+object DataModule {
+
+    fun load() {
+        loadKoinModules(postsModule())
+    }
+
+    private fun postsModule() : Module {
+        return module {
+            single<PostRepository> { PostRepositoryImpl(MockAPIService) }
+        }
+    }
+}
