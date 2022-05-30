18df16cd85637b8ed38d30d30d03c82516f3355a
Gabriel Fernandes Lemos <gabriel_lemos03@hotmail.com>
2022-05-29 22:30:36 -0300

    Remove comments and added koin module

---
+++ app/src/main/java/br/com/chicorialabs/astranovos/presentation/di/PresentationModule.kt
@@ -0,0 +1,20 @@
+package br.com.chicorialabs.astranovos.presentation.di
+
+import br.com.chicorialabs.astranovos.presentation.ui.home.HomeViewModel
+import org.koin.core.context.loadKoinModules
+import org.koin.core.module.Module
+import org.koin.dsl.module
+
+
+object PresentationModule {
+
+    fun load() {
+        loadKoinModules(viewModelModule())
+    }
+
+    private fun viewModelModule(): Module {
+        return module {
+            factory { HomeViewModel() }
+        }
+    }
+}
